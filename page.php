dcbd7034d654356a978722f03e1cf4ec6bdb7541
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-06-09 01:05:03 +0200

    Fix slider

--- page.php
+++ page.php
@@ -1,11 +1,7 @@
-<?php get_header(); ?>
-<div class="columns">
-	<main class="column is-8 is-offset-2">
-		<?php
-		the_post();
-		the_content();
-		?>
-	</main>
-</div>
 <?php
+get_header();
+
+the_post();
+the_content();
+
 get_footer();
