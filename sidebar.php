60fcd9614c986eebc6b092bdfaf7e93a8e789b0d
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-07-21 16:50:26 +0200

    Delete files

--- sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if ( is_active_sidebar( 'blog_sidebar' ) ) : ?>
-	<aside class="site-sidebar column is-4" data-component="sticky">
-		<?php dynamic_sidebar( 'blog_sidebar' ); ?>
-	</aside>
-	<?php
-endif;
