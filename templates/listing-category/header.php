e9b4c6e5b942c08e09fd31768ac39c33c5ef2842
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-07-22 13:18:17 +0200

    Fix styles

---
+++ templates/listing-category/header.php
@@ -0,0 +1,15 @@
+<section class="header-hero header-hero--large hp-listing-category hp-listing-category--view-page <?php if ( get_header_image() || get_term_meta( get_queried_object_id(), 'hp_image_id', true ) ) : ?>header-hero--cover<?php endif; ?>" data-component="parallax">
+	<div class="header-hero__content">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 col-xs-12">
+					<div class="hp-listing-category__header">
+						<div class="hp-listing-category__count"><?php printf( esc_html( _n( '%d Listing', '%d Listings', get_queried_object()->count, 'listinghive' ) ), number_format_i18n( get_queried_object()->count ) ); ?></div>
+						<h1 class="hp-listing-category__name"><?php single_cat_title(); ?></h1>
+						<div class="hp-listing-category__description"><?php echo category_description(); ?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
