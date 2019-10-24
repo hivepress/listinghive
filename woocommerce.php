903ab23b10fa31a8876ec065f3616412319d2ff0
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-10-24 22:08:32 +0300

    Fix WooCommerce compatibility

--- woocommerce.php
+++ woocommerce.php
@@ -1,17 +1,17 @@
 <?php
 get_header();
 
-if ( ! is_singular( 'product' ) ) :
+if ( ! is_product() ) :
 	?>
 	<div class="row">
 	<?php
 	echo hivetheme()->template->render_template( 'sidebar', [ 'sidebar' => 'shop' ] );
 endif;
 ?>
-<main <?php if ( ! is_singular( 'product' ) ) : ?>class="col-sm-8 col-xs-12 <?php if ( ! is_active_sidebar( 'shop' ) ) : ?>col-sm-offset-2<?php endif; ?>"<?php endif; ?>>
+<main <?php if ( ! is_product() ) : ?>class="col-sm-8 col-xs-12 <?php if ( ! is_active_sidebar( 'shop' ) ) : ?>col-sm-offset-2<?php endif; ?>"<?php endif; ?>>
 	<?php woocommerce_content(); ?>
 </main>
-<?php if ( ! is_singular( 'product' ) ) : ?>
+<?php if ( ! is_product() ) : ?>
 	</div>
 	<?php
 endif;
