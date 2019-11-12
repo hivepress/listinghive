27a407a736eb36394c6b1adc81272ac7b676bf44
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-12 21:52:49 +0200

    Remove demo import

--- includes/configs/plugins.php
+++ includes/configs/plugins.php
@@ -10,11 +10,6 @@ defined( 'ABSPATH' ) || exit;
 
 return [
 	[
-		'name' => 'One Click Demo Import',
-		'slug' => 'one-click-demo-import',
-	],
-
-	[
 		'name' => 'HivePress',
 		'slug' => 'hivepress',
 	],
