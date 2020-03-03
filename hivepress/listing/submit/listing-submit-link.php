942c1b6ea8400f7bbb4d9c29b781266d759c64a8
Ihor <ihor@hivepress.io>
2020-03-03 14:50:51 +0200

    Override listing submit button

---
+++ hivepress/listing/submit/listing-submit-link.php
@@ -0,0 +1,9 @@
+<?php
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( get_option( 'hp_listing_enable_submission' ) ) :
+	?>
+	<button type="button" class="hp-menu__item hp-menu__item--listing-submit button button--secondary" data-component="link" data-url="<?php echo esc_url( hivepress()->router->get_url( 'listing_submit_page' ) ); ?>"><i class="hp-icon fas fa-plus"></i><span><?php echo esc_html( hivepress()->translator->get_string( 'add_listing' ) ); ?></span></button>
+	<?php
+endif;
