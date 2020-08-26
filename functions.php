b26853228d9fb943e4fee569f432dd0e8dec2a67
Ihor <ihor@hivepress.io>
2020-08-26 12:27:49 +0300

    Remove framework files

--- functions.php
+++ functions.php
@@ -2,17 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Include the core class.
-require_once __DIR__ . '/includes/class-core.php';
-
-/**
- * Returns the core instance.
- *
- * @return HiveTheme\Core
- */
-function hivetheme() {
-	return HiveTheme\Core::instance();
-}
-
-// Initialize HiveTheme.
-hivetheme();
+// todo.
--- includes/configs/scripts.php
+++ includes/configs/scripts.php
@@ -4,29 +4,19 @@
  *
  * @package HiveTheme\Configs
  */
-
+// todo.
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
 return [
-	'focus_visible'  => [
-		'handle' => 'focus-visible',
-		'src'    => hivetheme()->get_url() . '/assets/js/focus-visible.min.js',
-	],
-
-	'fitvids'        => [
-		'handle' => 'fitvids',
-		'src'    => hivetheme()->get_url() . '/assets/js/jquery.fitvids.min.js',
-	],
-
-	'sticky_sidebar' => [
+	'sticky_sidebar'  => [
 		'handle' => 'sticky-sidebar',
-		'src'    => hivetheme()->get_url() . '/assets/js/jquery.sticky-sidebar.min.js',
+		'src'    => hivetheme()->get_url( 'listinghive' ) . '/assets/js/jquery.sticky-sidebar.min.js',
 	],
 
-	'core_frontend'  => [
-		'handle' => 'hivetheme-core-frontend',
-		'src'    => hivetheme()->get_url() . '/assets/js/frontend.min.js',
-		'deps'   => [ 'jquery', 'hoverIntent', 'imagesloaded', 'fitvids', 'sticky-sidebar', 'comment-reply' ],
+	'parent_frontend' => [
+		'handle' => 'hivetheme-parent-frontend',
+		'src'    => hivetheme()->get_url( 'listinghive' ) . '/assets/js/frontend.min.js',
+		'deps'   => [ 'hivetheme-core-frontend', 'sticky-sidebar' ],
 	],
 ];
