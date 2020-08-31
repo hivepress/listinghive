b70b4abfdf01ac180324efe5f4d2e1eb10e92fd6
Ihor <ihor@hivepress.io>
2020-08-31 23:16:09 +0300

    Fix template parts

--- templates/post/single/post-navigation.php
+++ templates/post/single/post-navigation.php
@@ -4,10 +4,10 @@ $next_post = get_next_post();
 
 if ( $prev_post || $next_post ) :
 	?>
-	<div class="post__navigation">
+	<div class="post-navbar">
 		<div class="row">
-			<div class="col-sm-6 col-xs-12"><?php if ( $prev_post ) : ?><h5><a href="<?php echo esc_url( get_permalink( $prev_post ) ); ?>"><i class="fas fa-chevron-left"></i><span><?php echo esc_html( get_the_title( $prev_post ) ); ?></span></a></h5><?php endif; ?></div>
-			<div class="col-sm-6 col-xs-12"><?php if ( $next_post ) : ?><h5><a href="<?php echo esc_url( get_permalink( $next_post ) ); ?>"><span><?php echo esc_html( get_the_title( $next_post ) ); ?></span><i class="fas fa-chevron-right"></i></a></h5><?php endif; ?></div>
+			<div class="post-navbar__start col-sm-6 col-xs-12"><?php if ( $prev_post ) : ?><div class="post-navbar__link"><h5><a href="<?php echo esc_url( get_permalink( $prev_post ) ); ?>"><i class="fas fa-chevron-left"></i><span><?php echo esc_html( get_the_title( $prev_post ) ); ?></span></a></h5></div><?php endif; ?></div>
+			<div class="post-navbar__end col-sm-6 col-xs-12"><?php if ( $next_post ) : ?><div class="post-navbar__link"><h5><a href="<?php echo esc_url( get_permalink( $next_post ) ); ?>"><span><?php echo esc_html( get_the_title( $next_post ) ); ?></span><i class="fas fa-chevron-right"></i></a></h5></div><?php endif; ?></div>
 		</div>
 	</div>
 	<?php
