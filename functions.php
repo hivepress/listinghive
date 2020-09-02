99326f16b3c5c8f958cacd3fe43b6b7d4b97c82b
Ihor <ihor@hivepress.io>
2020-09-02 12:55:54 +0300

    Fix loading framework

--- functions.php
+++ functions.php
@@ -3,6 +3,4 @@
 defined( 'ABSPATH' ) || exit;
 
 // Include the theme framework.
-if ( ! function_exists( 'hivetheme' ) ) {
-	require_once __DIR__ . '/vendor/hivepress/hivetheme/hivetheme.php';
-}
+require_once __DIR__ . '/vendor/hivepress/hivetheme/hivetheme.php';
