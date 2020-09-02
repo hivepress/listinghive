f378e2ed37d79241c305901f155d0d005f26c5ea
Ihor <ihor@hivepress.io>
2020-09-02 17:39:56 +0300

    Fix parallax

--- templates/page/page-header.php
+++ templates/page/page-header.php
@@ -1,4 +1,4 @@
-<section class="header-hero <?php echo esc_attr( $class ); ?>" <?php if ( get_theme_mod( 'header_image_parallax', true ) ) : ?>data-component="parallax"<?php endif; ?>>
+<section class="header-hero <?php echo esc_attr( $class ); ?>" <?php if ( get_theme_mod( 'header_image_parallax' ) ) : ?>data-component="parallax"<?php endif; ?>>
 	<div class="header-hero__content">
 		<div class="container">
 			<?php echo $content; ?>
