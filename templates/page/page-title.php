6ac2f68f43f5547bd14d0fac3bced857c9ac021e
Ihor <ihor@hivepress.io>
2021-11-18 20:05:48 +0200

    Fix page title

--- templates/page/page-title.php
+++ templates/page/page-title.php
@@ -1,5 +1,5 @@
 <div class="row">
 	<div class="col-sm-8 col-sm-offset-2 col-xs-12">
-		<h1 class="page__title"><?php the_title(); ?></h1>
+		<h1 class="page__title entry-title"><?php the_title(); ?></h1>
 	</div>
 </div>
