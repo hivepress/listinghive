ec89395df01645018fa773462d2bbac6886d7421
Ihor <ihor@hivepress.io>
2021-12-05 16:05:46 +0200

    Fix page titles

--- includes/components/class-theme.php
+++ includes/components/class-theme.php
@@ -139,26 +139,17 @@ final class Theme extends Component {
 				$classes[] = 'header-hero--title';
 			}
 
-			// Get page IDs.
-			$page_ids = [ absint( get_option( 'page_on_front' ) ) ];
+			// Check title.
+			$title = get_the_ID() !== absint( get_option( 'page_on_front' ) );
 
 			if ( ht\is_plugin_active( 'hivepress' ) ) {
-				$page_ids = array_merge(
-					$page_ids,
-					array_map(
-						'absint',
-						[
-							get_option( 'hp_page_listings' ),
-							get_option( 'hp_page_vendors' ),
-						]
-					)
-				);
+				$title = $title && ! hivepress()->request->get_context( 'post_query' );
 			}
 
 			// Render part.
 			if ( $content ) {
 				$output .= $content;
-			} elseif ( ! in_array( get_the_ID(), $page_ids, true ) ) {
+			} elseif ( $title ) {
 				$output .= hivetheme()->template->render_part( 'templates/page/page-title' );
 			}
 		} elseif ( is_singular( 'post' ) ) {
