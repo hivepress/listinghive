95ec5b82e0a364655afe9889b59f029642da7df0
HivePress <43493187+hivepress@users.noreply.github.com>
2024-09-16 20:30:11 +0200

    Allow HTML in category description

--- hivepress/listing-category/view/page/listing-category-header.php
+++ hivepress/listing-category/view/page/listing-category-header.php
@@ -11,7 +11,7 @@
 			?>
 			<h1 class="hp-listing-category__name"><?php echo esc_html( $listing_category->get_name() ); ?></h1>
 			<?php if ( $listing_category->get_description() ) : ?>
-				<div class="hp-listing-category__description"><?php echo esc_html( $listing_category->get_description() ); ?></div>
+				<div class="hp-listing-category__description"><?php echo $listing_category->display_description(); ?></div>
 			<?php endif; ?>
 		</div>
 	</div>
